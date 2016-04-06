8c9ddf0b970ba01799da5d8ea0c58223e0b88da8
Hugo Gonzalez <github@hugo.labkode.com>
2016-04-06 17:11:24 +0200

    Add Get prefix to User methods

--- entities.go
+++ entities.go
@@ -3,7 +3,7 @@ package entities
 // User represents an user of the system.
 // They are created by the authentication service.
 type User interface {
-	Username() string
-	Email() string
-	DisplayName() string
+	GetUsername() string
+	GetEmail() string
+	GetDisplayName() string
 }
