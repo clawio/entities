782867ebd8a8076c33c7d01a23d4296be1165e35
Hugo Gonzalez <github@hugo.labkode.com>
2016-04-22 14:23:07 +0200

    Add MetaData entity

--- entities.go
+++ entities.go
@@ -1,9 +1,31 @@
 package entities
 
-// User represents an user of the system.
-// They are created by the authentication service.
-type User interface {
-	GetUsername() string
-	GetEmail() string
-	GetDisplayName() string
-}
+const (
+	ObjectTypeTreeMimeType string = "clawio/tree"
+)
+const (
+	ObjectTypeTree ObjectType = iota
+	ObjectTypeBLOB
+)
+
+type (
+	// ObjectType indicates if the object is either a Tree or a BLOB.
+	ObjectType int
+
+	// Metadata represents the metadata information retrieved
+	// from an object, either tree or blob.
+	ObjectInfo interface {
+		GetPathSpec() string
+		GetSize() uint64
+		GetType() ObjectType
+		GetMimeType() string
+		GetChecksum() string
+	}
+	// User represents an user of the system.
+	// They are created by the authentication service.
+	User interface {
+		GetUsername() string
+		GetEmail() string
+		GetDisplayName() string
+	}
+)
