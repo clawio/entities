ad6d3db48db3ce51b684cca818326c2f37b534df
Hugo Gonzalez <github@hugo.labkode.com>
2016-04-24 22:57:24 +0200

    Add MockObjectInfo

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -1,6 +1,7 @@
 package mocks
 
 import (
+	"github.com/clawio/entities"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -29,3 +30,28 @@ func (m *MockUser) GetDisplayName() string {
 	args := m.Called()
 	return args.String(0)
 }
+
+// MockObjectInfo mocks an MockObjectInfo.
+type MockObjectInfo struct {
+	mock.Mock
+	Otype    entities.ObjectType `json:"type"`
+	PathSpec string              `json:"pathspec"`
+	Size     uint64              `json:"size"`
+	Checksum string              `json:"checksum"`
+	MimeType string              `json:"mime"`
+}
+
+//GetType mocks the GetType call.
+func (o *MockObjectInfo) GetType() entities.ObjectType { return o.Otype }
+
+// GetPathSpec mocks the GetPathSpec call.
+func (o *MockObjectInfo) GetPathSpec() string { return o.PathSpec }
+
+// GetSize mocks the GetSize call.
+func (o *MockObjectInfo) GetSize() uint64 { return o.Size }
+
+// GetChecksum mocks the GetChecksum call.
+func (o *MockObjectInfo) GetChecksum() string { return o.Checksum }
+
+// GetMimeType mocks the GetMimeType call.
+func (o *MockObjectInfo) GetMimeType() string { return o.MimeType }
